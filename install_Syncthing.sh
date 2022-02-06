3a659544c78b8d53cc93a5c722c9776010a43d88
Gabberted <mail@rakesh.info>
2022-02-06 18:15:11 +0100

    updated script with output and install service

--- install_Syncthing.sh
+++ install_Syncthing.sh
@@ -3,3 +3,19 @@ curl -s https://syncthing.net/release-key.txt | gpg --dearmor | sudo tee /usr/sh
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 sudo apt update
 sudo apt install syncthing
+echo "Running Syncthing.. for 10 seconds. Please hold."
+timeout 10s syncthing
+echo "Backup default config"
+mv ~/.config/syncthing/config.xml ~/.config/syncthing/config.xml.bak
+echo "Fixing webhost connection authorization"
+sed 's/127.0.0.1/0.0.0.0/g' ~/.config/syncthing/config.xml.bak > config.xml
+sudo systemctl enable syncthing@username.service
+echo "Service enabled"
+sudo systemctl start syncthing@username.service
+echo "Service started"
+
+
+
+
+
+
