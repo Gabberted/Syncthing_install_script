8dbe92b770d1551e2ee926ef8fb40fda5dd3c7fc
Gabberted <mail@rakesh.info>
2022-02-06 16:40:52 +0100

    second upload

---
+++ install_Syncthing.sh
@@ -0,0 +1,5 @@
+sudo apt install apt-transport-https
+curl -s https://syncthing.net/release-key.txt | gpg --dearmor | sudo tee /usr/share/keyrings/syncthing-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt update
+sudo apt install syncthing
